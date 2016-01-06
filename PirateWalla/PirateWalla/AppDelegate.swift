6e3155830ac922e53f51efcbfebf35d70fb2f1a2
Kevin Lohman <badpirate@gmail.com>
2016-01-05 22:21:31 -0800

    Initial Project

---
+++ PirateWalla/PirateWalla/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  PirateWalla
+//
+//  Created by Kevin Lohman on 1/5/16.
+//  Copyright © 2016 Logic High. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
